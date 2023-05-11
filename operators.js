febb7afd0d77b7e0a38b3515f768ce7c03f8881d
jesildebug <jesilpb95@gmail.com>
2023-05-11 11:43:46 +0530

    first commit

---
+++ operators.js
@@ -0,0 +1,38 @@
+// addition
+let x = 23
+let y = 34
+console.log(x+y);
+
+// substraction
+let a = 55
+let b = 36
+console.log(a-b);
+
+// maultiplication
+let m = 34
+let n = 25  
+console.log(m*n);
+
+// division
+
+let c = 15
+let d = 5
+console.log(c/d);
+
+// percentile
+
+let k =17
+let l = 3
+console.log(17%3);
+
+// operations
+let j = 45
+let o = 34
+let w = j+o
+let p = 23 
+let e = w*p
+let q = 3
+let r = e-q
+console.log(w);
+console.log(e);
+console.log(r);
