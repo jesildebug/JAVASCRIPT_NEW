3db746d965dec7e1ca34533cba5a25b6eb7de8ab
jesildebug <jesilpb95@gmail.com>
2023-05-14 09:22:24 +0530

    conditionOperators

---
+++ conditionOperator.js
@@ -0,0 +1,18 @@
+let a = 70
+let b = 80
+let c = 90
+let d = 50
+
+total_marks = a+b+c+d
+
+if("total_marks is > 250") {
+    console.log("Passed");
+} else {
+    console.log("failed");
+}
+
+
+let x = 9
+for(i=0;i<=10;i++) {
+console.log(i ,"*",x,"=",x*i);
+}
